4f6df6a70ede2354e55e6fd9af157ee30562d658
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-02-14 18:28:17 +0300

    Connect react and redux using provider

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './App.css';
-import Nav from './Nav';
+import store from './Redux/store';
+import Nav from './Components/Nav';
+import Models from './Components/Models';
 
 function App() {
   return (
+
     <Router>
-      <Nav />
-      <Routes>
-        <Route path="/" />
-        <Route path="/details" />
-      </Routes>
+      <Provider store={store}>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Models />} />
+          <Route path="/details" />
+        </Routes>
+      </Provider>
     </Router>
 
   );
