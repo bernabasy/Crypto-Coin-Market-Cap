6e9537eda56a9bf6aec2fb7fe137df7d79b631e1
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-02-17 12:58:02 +0300

    Create Details page for Crypto

---
+++ src/Components/DetailsPage.js
@@ -0,0 +1,59 @@
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchFinanceData } from '../Redux/Modeling';
+
+const Details = () => {
+  const cryptos = useSelector((state) => state.crypto);
+  const { id } = useParams();
+  const filter = cryptos.filter((crypto) => (crypto.id === id));
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (cryptos.length === 0) dispatch(fetchFinanceData());
+  }, [dispatch, cryptos.length]);
+
+  return (
+    <div>
+      <div className="flex bg-primary text-white h-100 w-100 border">
+        <div className=" w-100">
+          <img className="images ml-50 w-100 w" src={`https://www.cryptocompare.com${filter[0].img} `} alt="symbol" />
+        </div>
+        <div className="d-flex w justify-content-center">
+          <div>
+            <p>
+              <span className="m-3 border border-primary ">Name:</span>
+              {filter[0].Name}
+            </p>
+            <p />
+            <p>
+              <span className="m-3 border border-primary ">
+                AVERAGE PRICE:
+              </span>
+              {filter[0].price}
+            </p>
+            <p>
+              <span className="m-3 border border-primary ">
+                TODAYS LOWEST PRICE:
+              </span>
+              {filter[0].lowDay}
+            </p>
+            <p>
+              <span className="m-3 border border-primary ">
+                TODAYS Hights PRICE:
+              </span>
+              {filter[0].highDay}
+            </p>
+            <div className="gap-5  border border-primary ">
+              <span className="m-3 border border-primary ">
+                Algorithm:
+              </span>
+              { filter[0].algo }
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Details;
